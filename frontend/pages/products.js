4923bfd64fc655e4df745b4046e0022483bd3b8f
DamarisCaballero <113944199+DamarisCaballero@users.noreply.github.com>
2023-02-09 23:27:17 -0600

    Add files via upload

---
+++ frontend/pages/products.js
@@ -0,0 +1,9 @@
+import Products from '../components/Products';
+
+export default function ProductPage() {
+    return (
+      <div>
+        <Products />
+      </div>
+);
+}
---
+++ frontend/pages/sell.js
@@ -0,0 +1,9 @@
+import CreateProduct from '../components/CreateProduct';
+
+export default function SellPage() {
+  return (
+    <div>
+      <CreateProduct />
+    </div>
+  );
+}
